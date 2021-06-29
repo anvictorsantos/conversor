c7dd64311eeadc1e64c158af558e2014c3a2f494
Antonio Pontes <anvictorsantos@gmail.com>
2021-06-28 21:25:42 -0300

    new - first commit

---
+++ conversor/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">  
+    <h1>Conversor de moedas</h1>
+    <div class="linha">
+      <conversor moeda-a="USD" moedaB="BRL"></conversor>
+      <conversor moeda-a="BRL" moedaB="USD"></conversor>
+    </div>
+
+    <div class="linha">
+      <conversor moeda-a="CAD" moedaB="BRL"></conversor>
+      <conversor moeda-a="BRL" moedaB="CAD"></conversor>
+    </div>
+    
+    <div class="linha">
+      <conversor moeda-a="EUR" moedaB="BRL"></conversor>
+      <conversor moeda-a="BRL" moedaB="EUR"></conversor>
+    </div>
+  </div>
+</template>
+
+<script>
+import Conversor from './components/Conversor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: auto;
+  max-width: 900px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.linha {
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ conversor/src/components/Conversor.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="conversor">
+        <h2>{{ moedaA }} Para {{ moedaB }}</h2>
+        <input type="text" :placeholder="moedaA" v-model="moedaA_value">
+        <input type="button" value="Converter" v-on:click="converter">
+        <h2>{{ moedaB_value }}</h2>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'Conversor',
+    props: [
+        'moedaA',
+        'moedaB'
+    ],
+    data() {
+        return {
+            moedaA_value: '',
+            moedaB_value: 0
+        }
+    },
+    methods: {
+        converter() {
+            let de_para = this.moedaA + "_" + this.moedaB;
+            let url = "https://free.currconv.com/api/v7/convert?q=" + de_para + "&compact=ultra&apiKey=a6f98962807910b11324";
+
+        fetch(url)
+            .then(response => {
+                console.log(url)
+                return response.json();
+            })
+            .then(json => {
+                console.log(json[de_para])
+                let cotacao = json[de_para];
+                this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
+            });
+        }
+    },
+}
+</script>
+
+<style scoped>
+.conversor {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    padding: 20px;
+    max-width: 300px;
+}
+</style>
